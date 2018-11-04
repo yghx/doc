64435a0a56e828836b63184e0e1b46855afc32fb
yghx <983964803@qq.com>
2018-11-05 00:10:23 +0800

    '相关学习文档'

---
+++ spring-boot/从零开始学Spring Boot/3c0307ef-d9cb-3fa2-9c68-0b47335e82f6/spring-boot-hello1/src/main/java/com/kfit/test/service/DemoService.java
@@ -0,0 +1,35 @@
+package com.kfit.test.service;
+
+import javax.annotation.Resource;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.kfit.test.bean.Demo;
+import com.kfit.test.dao.DemoDao;
+import com.kfit.test.dao.DemoRepository;
+
+/**
+ * 提供Demo服务类.
+ * @author Administrator
+ *
+ */
+@Service
+public class DemoService {
+	
+	@Resource
+	private DemoRepository demoRepository;
+	
+	@Resource
+	private DemoDao demoDao;
+	
+	@Transactional
+	public void save(Demo demo){
+		demoRepository.save(demo);
+	}
+	
+	public Demo getById(long id){
+		demoRepository.findOne(id);//在demoRepository可以直接使用findOne进行获取.
+		return demoDao.getById(id);
+	}
+}
